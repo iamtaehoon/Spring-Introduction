0297cde576a4c320cccbb870a011b804da2b39c7
iamtaehoon <kimth9981@naver.com>
2022-01-19 09:21:26 +0900

    refactor(): MemoryMemberRepo HashMap으로 변경

--- src/main/java/com/example/introduction/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/introduction/repository/MemoryMemberRepository.java
@@ -1,35 +1,38 @@
 package com.example.introduction.repository;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import com.example.introduction.domain.Member;
 
 public class MemoryMemberRepository implements MemberRepositoryInterface {
-    private List<Member> memberRepository = new ArrayList<>();
-    private Long sequence = -1L;
+    private Map<Long, Member> memberRepository = new HashMap();
+    private Long sequence = 0L;
     @Override
     public Long save(Member member) {
         sequence += 1;
         member.putIdOnMember(sequence);
-        memberRepository.add(member); // 동일 객체가 있는지 검증은 서비스단계가 맞음. 여기가 아님.
+        memberRepository.put(sequence, member); // 동일 객체가 있는지 검증은 서비스단계가 맞음. 여기가 아님.
         return member.getId();
     }
 
     @Override
     public Optional<Member> findById(Long memberId) {
-        return Optional.ofNullable(memberRepository.get(memberId.intValue()));
+        return Optional.ofNullable(memberRepository.get(memberId));
     }
 
     @Override
     public Optional<Member> findByName(String memberName) {
-        return memberRepository.stream().filter(member -> member.getName().equals(memberName)).findAny();
+        return memberRepository.values().stream().filter(member -> member.getName().equals(memberName)).findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return Collections.unmodifiableList(memberRepository);
+        return new ArrayList<>(memberRepository.values());
     }
 }
